f53cd8b9d98531e35d4e0290b712ba1407ea622b
MinaRomany-dev <minaromanyfawzy@gmail.com>
2026-03-01 18:56:07 +0200

    after add readme & ignore

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    News(),
+  );
+}
+
+class News extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+      },
+      );
+  }
+}
